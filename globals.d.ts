a9580d60ace57ccdcdc66268ca59845bab1d7822
Amir Bandeali <abandeali1@gmail.com>
2017-07-20 09:02:31 -0700

    Add RPC utils

--- globals.d.ts
+++ globals.d.ts
@@ -1,3 +1,4 @@
+declare module 'es6-promisify';
 // Truffle injects the following into the global scope
 declare var web3: any; // TODO: figure out how to use Web3 definition from within global.d.ts instead of `any`
 declare var artifacts: any;
--- test/vesting_wallet.ts
+++ test/vesting_wallet.ts
@@ -1,16 +1,22 @@
 import * as assert from 'assert';
 import * as BigNumber from 'bignumber.js';
+import * as Web3 from 'web3';
+import * as promisify from 'es6-promisify';
 import {Artifacts} from '../util/artifacts';
 import {testUtil} from '../util/test_util';
+import {RPC} from '../util/rpc';
 import {TokenMetadata, ContractInstance, ContractResponse} from '../util/types';
 
 const {VestingWallet, DummyToken} = new Artifacts(artifacts);
+
+const web3Instance: Web3 = web3;
 
 contract('VestingWallet', (accounts: string[]) => {
     const owner = accounts[0];
     const registeredAddress = accounts[1];
 
     const vestingDurationInSec = 10000;
+    const rpc: RPC = new RPC();
 
     let vestingWallet: ContractInstance;
     let vestingToken: ContractInstance;
@@ -41,12 +47,13 @@ contract('VestingWallet', (accounts: string[]) => {
 
         it('should throw if not called by owner', async () => {
             try {
+                const notOwner = registeredAddress;
                 await vestingWallet.registerVestingSchedule(_addressToRegister,
                                                             _depositor,
                                                             _startTimeInSec,
                                                             _endTimeInSec,
                                                             _totalAmount,
-                                                            {from: registeredAddress});
+                                                            {from: notOwner});
                 throw new Error('registerVestingSchedule succeeded when it should have failed');
             } catch (err) {
                 testUtil.assertThrow(err);
@@ -160,6 +167,54 @@ contract('VestingWallet', (accounts: string[]) => {
             } catch (err) {
                 testUtil.assertThrow(err);
             }
+        });
+    });
+
+    describe('withdraw', () => {
+        const _addressToRegister = registeredAddress;
+        const _depositor = owner;
+        const _totalAmount = vestingTokenMetadata.totalSupply;
+
+        beforeEach(async () => {
+            const blockNum = await promisify(web3Instance.eth.getBlockNumber)();
+            const blockData = await promisify(web3Instance.eth.getBlock)(blockNum);
+            const blockTimestamp: number = blockData.timestamp;
+
+            const _startTimeInSec = new BigNumber(blockTimestamp);
+            const _endTimeInSec = _startTimeInSec.plus(vestingDurationInSec);
+
+            await vestingWallet.registerVestingSchedule(_addressToRegister,
+                                                        _depositor,
+                                                        _startTimeInSec,
+                                                        _endTimeInSec,
+                                                        _totalAmount,
+                                                        {from: owner});
+        });
+
+        it('throw if called from an unregistered address', async () => {
+            try {
+                const unregisteredAddress = owner;
+                await vestingWallet.withdraw({from: unregisteredAddress});
+                throw new Error('withdraw succeeded when it should have failed');
+            } catch (err) {
+                testUtil.assertThrow(err);
+            }
+        });
+
+        it('should allow a registered address to withdraw vested tokens', async () => {
+            const percentageVested = .5;
+            const timeToIncreaseInSec = percentageVested * vestingDurationInSec;
+            await rpc.increaseTimeAsync(timeToIncreaseInSec);
+
+            await vestingWallet.withdraw({from: registeredAddress});
+            const registeredAddressBalance = await vestingToken.balanceOf(registeredAddress);
+            const vestingWalletBalance = await vestingToken.balanceOf(vestingWallet.address);
+
+            const expectedRegisteredAddressBalance = new BigNumber(_totalAmount).times(percentageVested);
+            const expectedVestingWalletBalance = new BigNumber(_totalAmount).times(1 - percentageVested);
+
+            assert.equal(registeredAddressBalance.toString(), expectedRegisteredAddressBalance.toString());
+            assert.equal(vestingWalletBalance.toString(), expectedVestingWalletBalance.toString());
         });
     });
 });
